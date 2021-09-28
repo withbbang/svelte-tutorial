e83332258aad7d242bd62509554419bb6e7bfecc
Bread <withbbang@gmail.com>
2021-09-28 23:56:54 +0900

    before check select componenet logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
-	import Calculator from './components/Calculator.svelte';
-	import TodoList from './components/TodoList.svelte';
+  import Calculator from "./components/Calculator.svelte";
+  import TodoList from "./components/TodoList.svelte";
+  import Select from "./components/Select.svelte";
+
+  const options = [
+    "",
+    "Red",
+    { label: "Green" },
+    { label: "Blue", value: "b" },
+  ];
+
+  let selected;
+
+  const handleSelect = (event) => (selected = event.detail);
 </script>
 
-<div>
-	<!-- <Calculator/> -->
-	<TodoList/>
-</div>
+<!-- <Calculator/> -->
+<!-- <TodoList/> -->
+<Select {options} on:select={handleSelect} />
+{#if selected}
+  <div>You selected {selected}.</div>
+{/if}
--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,28 +1,29 @@
 <script>
-    import {createEventDispatcher} from 'svelte';
-    import {fade} from 'svelte/transition';
+  import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
 
-    const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-    export let todo;
+  export let todo;
 </script>
 
 <li transition:fade>
-    <input
-        type="checkbox"
-        checked={todo.done}
-        on:change={() => dispatch('toggleDone')}/>
-    <span class={'done-' + todo.done}>{todo.text}</span>
-    <button on:click={() => dispatch('delete')}>Delete</button>
+  <input
+    type="checkbox"
+    checked={todo.done}
+    on:change={() => dispatch("toggleDone")}
+  />
+  <span class={"done-" + todo.done}>{todo.text}</span>
+  <button on:click={() => dispatch("delete")}>Delete</button>
 </li>
 
 <style>
-    .done-true{
-        color: gray;
-        text-decoration: line-through;
-    }
+  .done-true {
+    color: gray;
+    text-decoration: line-through;
+  }
 
-    li{
-        margin-top: 5px;
-    }
+  li {
+    margin-top: 5px;
+  }
 </style>
